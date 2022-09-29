1c0c83b63abbc58ad6c8b3f509b8751a6c8f6126
Glitch (glitch-hello-vue) <none>
2022-09-29 09:55:29 +0000

    🐕🌂 Checkpoint ./src/main.js:1403779/8235

--- src/main.js
+++ src/main.js
@@ -1,45 +1,52 @@
 // Model
-const todos = 
+let todos = [];
+let newLabel = "";
 
+// Actions
+function addTodo() {
+  todos.push({ label: newLabel, done: false });
+  newLabel = "";
+  render();
+}
 
+function removeTodo(todo) {
+  todo.done = false;
+  render();
+}
+
+function updateNewLabel(_newLabel) {
+  newLabel = _newLabel
+  render();
+}
 
 // View
 let ul = document.querySelector("ul");
 let input = document.querySelector("input");
 let button = document.querySelector("button");
 
-// Actions
-function addItem(label) {
-  todos.push({ label, done : false})
-  render()
-}
-
 function render() {
-  ul.innerHTML = ""
+  button.textContent = newLabel
   
-  for(let todo of todos) {
-    
+  ul.innerHTML = "";
+
+  input.onchange = () => updateNewLabel(input.value)
+
+  for (let todo of todos) {
+    let item_li = document.createElement("LI");
+
+    let item_span = document.createElement("SPAN");
+    item_span.textContent = todo.label;
+    item_span.style.textDecoration = todo.done ? "line-through" : "";
+    item_li.appendChild(item_span);
+
+    let item_button = document.createElement("BUTTON");
+    item_button.onclick = () => removeTodo(todo);
+    item_li.appendChild(item_button);
   }
+
+  input.value = newLabel;
   
+  button.onclick = () => addTodo()
 }
 
-render()
-
-button.onclick = () => {
-  let item_li = document.createElement("LI");
-
-  let item_span = document.createElement("SPAN");
-  item_span.textContent = input.value;
-  item_li.appendChild(item_span);
-
-  let item_button = document.createElement("BUTTON");
-  item_button.textContent = "X";
-  item_button.onclick = () => {
-    item_span.style.textDecoration = "line-through";
-  };
-  item_li.appendChild(item_button);
-
-  ul.appendChild(item_li);
-
-  input.value = "";
-};
+render();
